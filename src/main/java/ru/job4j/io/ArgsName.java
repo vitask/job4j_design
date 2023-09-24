c86f0f430b8fe1f3863b0a469e12daabbe737d32
vitask <skalenko.v@yandex.ru>
2023-09-24 14:49:32 +0700

    13. Именованные аргументы [#295518]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,57 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException(String.format(
+                    "This key: '%s' is missing", key
+            ));
+        }
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Arguments not passed to program");
+        }
+        for (String str : args) {
+            int index = check(str);
+            values.put(str.substring(1, index), str.substring(index + 1));
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName argsName = new ArgsName();
+        argsName.parse(args);
+        return argsName;
+    }
+
+    private static int check(String arg) {
+        if (!arg.startsWith("-")) {
+            throw new IllegalArgumentException(String.format(
+                    "Error: This argument '%s' does not start with a '-' character", arg
+            ));
+        }
+        int index = arg.indexOf("=");
+        if (index < 2 || index >= arg.length() - 1) {
+            throw new IllegalArgumentException(String.format(
+                    "Error: This argument '%s' does not contain an equal sign", arg
+            ));
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[]{"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
