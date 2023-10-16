c624d7aa02fd9bd67a38ef932930402a09890aa7
vitask <skalenko.v@yandex.ru>
2023-10-16 19:42:42 +0700

    2. Что такое Socket? [#4850 #452908]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,27 +1,29 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
-            while (!server.isClosed()) {
+            boolean complete = true;
+            while (complete) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
-                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                     BufferedReader in = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream()))) {
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         System.out.println(str);
-                        if (str.toLowerCase().contains("?msg=bye")) {
-                            server.close();
+                        if (str.contains("msg=Bye")) {
+                            complete = false;
                         }
                     }
-                    out.flush();
+                    if (complete) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    } else {
+                        out.write("HTTP/1.1 400 OK\r\n\r\n".getBytes());
+                    }
                 }
             }
         }
