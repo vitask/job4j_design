073fe65561779abc7947bdbb5499963ef7b8cc8a
vitask <skalenko.v@yandex.ru>
2023-10-16 23:51:20 +0700

    2. Что такое Socket? [#4850 #452908]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -13,18 +13,13 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        System.out.println(str);
-                        if (str.contains("msg=Bye")) {
-                            complete = false;
-                        }
-                    }
-                    if (complete) {
-                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    } else {
-                        out.write("HTTP/1.1 400 OK\r\n\r\n".getBytes());
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    String str = in.readLine();
+                    if (str.contains("msg=Bye")) {
+                        complete = false;
                     }
                 }
+
             }
         }
     }
