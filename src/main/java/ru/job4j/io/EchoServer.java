b74459e1d4cb14b31876008d3a5ab11fce5a63c9
vitask <skalenko.v@yandex.ru>
2023-10-17 00:02:51 +0700

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -15,8 +15,13 @@ public class EchoServer {
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     String str = in.readLine();
-                    if (str.contains("msg=Bye")) {
+                    System.out.println(str);
+                    if (str.contains("?msg=Exit")) {
                         complete = false;
+                    } else if (str.contains("?msg=Hello")) {
+                        out.write("Hello".getBytes());
+                    } else if (str.contains("?msg")) {
+                        out.write("What".getBytes());
                     }
                 }
 
