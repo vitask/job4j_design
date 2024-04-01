0d22ec37cea704ad803176aa19de3a024adbcaf5
vitask <skalenko.v@yandex.ru>
2024-04-02 00:22:09 +0700

    0. JDBC [#6863]

---
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -0,0 +1,24 @@
+package ru.job4j.jdbc;
+
+import ru.job4j.io.Config;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDemo {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Config config = new Config("./data/app.properties");
+        config.load();
+        Class.forName(config.value("driver"));
+        try (Connection connection = DriverManager.getConnection(
+                config.value("url"),
+                config.value("login"),
+                config.value("password"))) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getUserName());
+            System.out.println(metaData.getURL());
+        }
+    }
+}
