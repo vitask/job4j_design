8782c67471d79ed293ad1d89405aef97f50ccb4b
vitask <skalenko.v@yandex.ru>
2024-06-04 22:44:55 +0700

    4. Найти утечку памяти. [#504882]

---
+++ src/main/java/ru/job4j/gc/leak/Post.java
@@ -0,0 +1,38 @@
+package ru.job4j.gc.leak;
+
+import java.util.List;
+
+public class Post {
+
+    private Integer id;
+
+    private String text;
+
+    private final List<Comment> comments;
+
+    public Post(String text, List<Comment> comments) {
+        this.text = text;
+        this.comments = comments;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+}
