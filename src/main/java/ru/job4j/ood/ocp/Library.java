8ceb61f226564ee5454f2b4aef79c9d09af8ca6c
vitask <skalenko.v@yandex.ru>
2024-06-25 00:05:46 +0700

    0. Принцип открытости закрытости [#4914 #502212]

--- src/main/java/ru/job4j/ood/ocp/Library.java
+++ src/main/java/ru/job4j/ood/ocp/Library.java
@@ -4,11 +4,11 @@ public class Library {
 
     public void takeBook(String book) {
         if (book.equals("роман")) {
-
+            System.out.println("взял книку жанра роман");
         } else if (book.equals("фантастика")) {
-
+            System.out.println("взял книку жанра фантастика");
         } else if (book.equals("детектив")) {
-
+            System.out.println("взял книку жанра детектив");
         }
     }
 }
--- src/main/java/ru/job4j/ood/ocp/Payment.java
+++ src/main/java/ru/job4j/ood/ocp/Payment.java
@@ -3,9 +3,9 @@ package ru.job4j.ood.ocp;
 public class Payment {
     public void paymentProcessor(String paymentType) {
         if (paymentType.equals("Qiwi")) {
-            
+            System.out.println("Выполняется обработка платежа с помощью системы Qiwi");
         } else if (paymentType.equals("Мир")) {
-            
+            System.out.println("Выполняется обработка платежа с помощью банковской карты Мир");
         }
     }
 }
