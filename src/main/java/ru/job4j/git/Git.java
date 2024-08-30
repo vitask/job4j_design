b0b0d3e05059fb08eae27fdfd61938ef645075a1
vitask <skalenko.v@yandex.ru>
2024-08-30 18:28:34 +0700

    3.2. Откат изменений [#504830]

--- src/main/java/ru/job4j/git/Git.java
+++ src/main/java/ru/job4j/git/Git.java
@@ -3,5 +3,7 @@ package ru.job4j.git;
 public class Git {
     public static void main(String[] args) {
         System.out.println("Git basics");
+        System.out.println("First commit");
+        System.out.println("Second commit");
     }
 }
